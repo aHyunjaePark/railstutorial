29e835663575ed1e3997b0717c96f8a345eadb01
aHyunjaePark <hyunjae.park@access-company.com>
2014-12-08 13:40:43 +0000

    chapter 8 complete

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  before_save :email_downcase
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :password_digest, length: {minimum: 6}
+  has_secure_password
+  private
+  def email_downcase
+    self.email = self.email.downcase
+  end
+end
